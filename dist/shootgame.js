0fd371c8ea49903aa97c0e590f1a415d4bd978b7
CodeMode365 <codemode365@gmail.com>
2022-11-20 19:31:45 +0000

    raven game

--- dist/shootgame.js
+++ dist/shootgame.js
@@ -1,16 +1,58 @@
 "use strict";
-const collisionCanvas = document.getElementById("collisionCanvas");
-const collisionCTX = collisionCanvas.getContext("2d");
-collisionCanvas.width = window.innerWidth;
-collisionCanvas.height = window.innerHeight;
 const canvaS = document.getElementById("shootGame");
 const ctxS = canvaS.getContext("2d");
 canvaS.width = window.innerWidth;
 canvaS.height = window.innerHeight;
+const collisionCanvas = document.getElementById("collisionCanvas");
+const collisionCTX = collisionCanvas.getContext("2d");
+collisionCanvas.width = window.innerWidth;
+collisionCanvas.height = window.innerHeight;
 let timeToNextRaven = 0;
 const ravenIntarval = 500;
 let lastTime = 0;
-const score = 0;
+let score = 0;
+let gameOver = false;
+const explozers = [];
+let highScore;
+if (localStorage.getItem("highScore")) {
+    highScore = parseInt(localStorage.getItem("highScore"));
+}
+else {
+    localStorage.setItem("highScore", "0");
+    highScore = 0;
+}
+class Explosion {
+    constructor(x, y, size) {
+        this.spriteHeight = 179;
+        this.spriteWidth = 200;
+        this.explodeImage = new Image();
+        this.explodeEffect = new Audio();
+        this.frame = 0;
+        this.timeSinceLastFrame = 0;
+        this.frameInterval = 100;
+        this.markedForDeletion = false;
+        this.x = x;
+        this.y = y;
+        this.explodeImage.src = "../assets/boom.png";
+        this.explodeEffect.src = "../assets/sounds/gun.mp3";
+        this.size = size;
+    }
+    update(deltaTime) {
+        if (this.frame === 0)
+            this.explodeEffect.play();
+        this.timeSinceLastFrame += deltaTime;
+        if (this.timeSinceLastFrame > this.frameInterval) {
+            this.frame++;
+            this.timeSinceLastFrame = 0;
+            if (this.frame > 5) {
+                this.markedForDeletion = true;
+            }
+        }
+    }
+    draw() {
+        ctxS.drawImage(this.explodeImage, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y - this.size / 4, this.size, this.size);
+    }
+}
 let ravens = [];
 class Raven {
     constructor() {
@@ -30,8 +72,8 @@ class Raven {
         this.creatureImage.src = "../assets/raven.png";
         this.timeSinceFlap = 0;
         this.flapInterval = Math.random() * 50 + 50;
-        this.randmColor = [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)];
-        this.GenColor = `rgba(${this.randmColor[0]},${this.randmColor[1]},${this.randmColor[2]},${this.randmColor[3]})`;
+        this.randmColor = [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)];
+        this.GenColor = `rgba(${this.randmColor[0]},${this.randmColor[1]},${this.randmColor[2]})`;
     }
     update(deltaTime) {
         this.x -= this.directionX;
@@ -52,6 +94,8 @@ class Raven {
         if (this.x < 0 - this.width) {
             this.markedForDeletion = true;
         }
+        if (this.x < 0 - this.width)
+            gameOver = true;
     }
     draw() {
         collisionCTX.fillStyle = this.GenColor;
@@ -60,14 +104,49 @@ class Raven {
     }
 }
 function drawScore() {
-    ctxS.fillStyle = "white";
+    ctxS.font = "50px Impact";
+    ctxS.fillStyle = "black";
     ctxS.fillText("Score: " + score, 50, 75);
-    ctxS.fillStyle = "whtie";
-    ctxS.fillText("Score: " + score, 55, 80);
+    ctxS.fillStyle = "white";
+    ctxS.fillText("Score: " + score, 52, 76);
+    ctxS.fillText("Score: " + score, 53, 76);
+    ctxS.fillStyle = "black";
+    ctxS.fillText("Score: " + score, 54, 76);
+    ctxS.fillStyle = "black";
+    ctxS.font = "30px Impact";
+    ctxS.fillText("Hight Score: " + highScore, 54, 136);
+}
+function GAME_OVER() {
+    ctxS.textAlign = "center";
+    ctxS.font = "80px Impact";
+    ctxS.fillStyle = "black";
+    ctxS.fillText("GAME OVER", canvaS.width / 2 - 4, canvaS.height / 2 - 4);
+    ctxS.fillStyle = "white";
+    ctxS.fillText("GAME OVER", canvaS.width / 2 - 2, canvaS.height / 2 - 2);
+    ctxS.fillStyle = "rgb(230,120,60)";
+    ctxS.fillText("GAME OVER", canvaS.width / 2, canvaS.height / 2);
+    ctxS.fillStyle = "black";
+    ctxS.fillText("Your Score is: " + score, canvaS.width / 2 - 4, canvaS.height / 2 + 80 - 4);
+    ctxS.fillStyle = "white";
+    ctxS.fillText("Your Score is: " + score, canvaS.width / 2 - 2, canvaS.height / 2 + 80 - 2);
+    ctxS.fillStyle = "rgb(64,134,74)";
+    ctxS.fillText("Your Score is: " + score, canvaS.width / 2, canvaS.height / 2 + 80);
+    if (score >= highScore) {
+        localStorage.setItem("highScore", score.toLocaleString());
+    }
 }
 window.addEventListener("click", (e) => {
-    const deltectPixelColor = ctxS.getImageData(e.x, e.y, 1, 1);
-    console.log(deltectPixelColor);
+    const deltectPixelColor = collisionCTX.getImageData(e.x, e.y, 1, 1);
+    const pc = deltectPixelColor.data;
+    ravens.forEach((raven) => {
+        if (raven.randmColor[0] == Math.floor(pc[0]) && raven.randmColor[1] == Math.floor(pc[1]) && raven.randmColor[2] == Math.floor(pc[2])) {
+            raven.markedForDeletion = true;
+            score++;
+            explozers.push(new Explosion(raven.x, raven.y, raven.width));
+        }
+    });
+    if (score > highScore)
+        highScore = score;
 });
 function animate(timestamep) {
     ctxS.clearRect(0, 0, canvaS.width, canvaS.height);
@@ -84,13 +163,16 @@ function animate(timestamep) {
             return a.width - b.width;
         });
     }
-    [...ravens].forEach((raven) => {
-        raven.update(deltaTime);
+    [...ravens, ...explozers].forEach((object) => {
+        object.update(deltaTime);
     });
-    [...ravens].forEach((raven) => {
-        raven.draw();
+    [...ravens, ...explozers].forEach((object) => {
+        object.draw();
     });
     ravens = ravens.filter((raven) => !raven.markedForDeletion);
-    requestAnimationFrame(animate);
+    if (!gameOver)
+        requestAnimationFrame(animate);
+    if (gameOver)
+        GAME_OVER();
 }
 animate(0);
--- src/shootgame.ts
+++ src/shootgame.ts
@@ -1,10 +1,5 @@
 
-//canvas for detecting collition
-const collisionCanvas: HTMLCanvasElement = document.getElementById("collisionCanvas")
-const collisionCTX: CanvasRenderingContext2D = collisionCanvas.getContext("2d")
 
-collisionCanvas.width = window.innerWidth
-collisionCanvas.height = window.innerHeight
 
 //main canavs
 const canvaS: HTMLCanvasElement = document.getElementById("shootGame")
@@ -13,17 +8,79 @@ const ctxS: CanvasRenderingContext2D = canvaS.getContext("2d")
 canvaS.width = window.innerWidth
 canvaS.height = window.innerHeight
 
+//canvas for detecting collition
+const collisionCanvas: HTMLCanvasElement = document.getElementById("collisionCanvas")
+const collisionCTX: CanvasRenderingContext2D = collisionCanvas.getContext("2d")
+
+collisionCanvas.width = window.innerWidth
+collisionCanvas.height = window.innerHeight
+
+
+
+//reavens
 let timeToNextRaven: number = 0
 const ravenIntarval: number = 500
 let lastTime: number = 0
-const score = 0
+let score = 0
+let gameOver: boolean = false
+//all explosives
+const explozers: Explosion[] = [
+]
+let highScore: number;
+//setLocal storage value for hight score
+if (localStorage.getItem("highScore")) {
+    // console.log(localStorage.getItem('highScore'))
+    highScore = parseInt(localStorage.getItem("highScore"))
 
+} else {
+    localStorage.setItem("highScore", "0")
+    highScore = 0
+}
+
+class Explosion {
+    //setting up explosives value
+    private spriteHeight = 179
+    private spriteWidth = 200
+    private explodeImage: HTMLImageElement = new Image()
+    private explodeEffect: HTMLAudioElement = new Audio()
+    private x: number
+    private y: number
+    private size: number
+    private frame = 0
+    private timeSinceLastFrame = 0
+    private frameInterval = 100
+    private markedForDeletion = false
+    constructor(x: number, y: number, size: number) {
+        this.x = x
+        this.y = y
+        this.explodeImage.src = "../assets/boom.png"
+        this.explodeEffect.src = "../assets/sounds/gun.mp3"
+        this.size = size
+    }
+    public update(deltaTime: number): void {
+        if (this.frame === 0) this.explodeEffect.play()
+        this.timeSinceLastFrame += deltaTime
+
+        //increaing the frame picture slowly
+        if (this.timeSinceLastFrame > this.frameInterval) {
+            this.frame++
+            this.timeSinceLastFrame = 0
+            if (this.frame > 5) { this.markedForDeletion = true }
+        }
+
+    }
+    public draw(): void {
+        ctxS.drawImage(this.explodeImage, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y - this.size / 4, this.size, this.size)
+    }
+}
+
+//ravens
 let ravens: Raven[] = []
 class Raven {
     readonly width: number
     private height: number
-    private x: number
-    private y: number
+    public x: number
+    public y: number
     private directionX: number
     private directionY: number
     private creatureImage: HTMLImageElement = new Image()
@@ -34,7 +91,7 @@ class Raven {
     private maxFrame: number = 4
     private timeSinceFlap: number
     private flapInterval: number
-    private randmColor: number[]
+    readonly randmColor: number[]
     private GenColor: string
     public markedForDeletion: boolean
     constructor() {
@@ -51,8 +108,8 @@ class Raven {
         this.creatureImage.src = "../assets/raven.png"
         this.timeSinceFlap = 0
         this.flapInterval = Math.random() * 50 + 50
-        this.randmColor = [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)]
-        this.GenColor = `rgba(${this.randmColor[0]},${this.randmColor[1]},${this.randmColor[2]},${this.randmColor[3]})`
+        this.randmColor = [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)]
+        this.GenColor = `rgba(${this.randmColor[0]},${this.randmColor[1]},${this.randmColor[2]})`
     }
     public update(deltaTime: number): void {
         this.x -= this.directionX
@@ -75,7 +132,7 @@ class Raven {
         if (this.x < 0 - this.width) {
             this.markedForDeletion = true
         }
-
+        if (this.x < 0 - this.width) gameOver = true
     }
     //draw the raven
     public draw(): void {
@@ -88,16 +145,52 @@ class Raven {
 
 //scoring
 function drawScore(): void {
-    ctxS.fillStyle = "white";
+    // ctxS.font = "30px Arial"
+    ctxS.font = "50px Impact"
+    ctxS.fillStyle = "black";
     ctxS.fillText("Score: " + score, 50, 75)
-    ctxS.fillStyle = "whtie"
-    ctxS.fillText("Score: " + score, 55, 80)
-
+    ctxS.fillStyle = "white"
+    ctxS.fillText("Score: " + score, 52, 76)
+    ctxS.fillText("Score: " + score, 53, 76)
+    ctxS.fillStyle = "black"
+    ctxS.fillText("Score: " + score, 54, 76)
+    ctxS.fillStyle = "black"
+    ctxS.font = "30px Impact"
+    ctxS.fillText("Hight Score: " + highScore, 54, 136)
 }
+function GAME_OVER(): void {
+    ctxS.textAlign = "center"
+    ctxS.font = "80px Impact"
+    ctxS.fillStyle = "black"
+    ctxS.fillText("GAME OVER", canvaS.width / 2 - 4, canvaS.height / 2 - 4)
+    ctxS.fillStyle = "white"
+    ctxS.fillText("GAME OVER", canvaS.width / 2 - 2, canvaS.height / 2 - 2)
+    ctxS.fillStyle = "rgb(230,120,60)"
+    ctxS.fillText("GAME OVER", canvaS.width / 2, canvaS.height / 2)
+    ctxS.fillStyle = "black"
+    ctxS.fillText("Your Score is: " + score, canvaS.width / 2 - 4, canvaS.height / 2 + 80 - 4)
+    ctxS.fillStyle = "white"
+    ctxS.fillText("Your Score is: " + score, canvaS.width / 2 - 2, canvaS.height / 2 + 80 - 2)
+    ctxS.fillStyle = "rgb(64,134,74)"
+    ctxS.fillText("Your Score is: " + score, canvaS.width / 2, canvaS.height / 2 + 80)
+    if (score >= highScore) {
+        localStorage.setItem("highScore", score.toLocaleString())
+    }
+}
+
 //clicking event
 window.addEventListener("click", (e: MouseEvent) => {
-    const deltectPixelColor = ctxS.getImageData(e.x, e.y, 1, 1)
-    console.log(deltectPixelColor)
+    const deltectPixelColor = collisionCTX.getImageData(e.x, e.y, 1, 1)
+    //to hold pixels color
+    const pc = deltectPixelColor.data
+    ravens.forEach((raven: Raven): void => {
+        if (raven.randmColor[0] == Math.floor(pc[0]) && raven.randmColor[1] == Math.floor(pc[1]) && raven.randmColor[2] == Math.floor(pc[2])) {
+            raven.markedForDeletion = true
+            score++
+            explozers.push(new Explosion(raven.x, raven.y, raven.width))
+        }
+    })
+    if (score > highScore) highScore = score
 })
 
 //to run the animation
@@ -118,15 +211,15 @@ function animate(timestamep: number): void {
             return a.width - b.width;
         })
     }
-    [...ravens].forEach((raven: Raven): void => {
-        raven.update(deltaTime)
+    [...ravens, ...explozers].forEach((object: (Raven | Explosion)): void => {
+        object.update(deltaTime)
     });
-    [...ravens].forEach((raven: Raven): void => {
-        raven.draw()
+    [...ravens, ...explozers].forEach((object: (Raven | Explosion)): void => {
+        object.draw()
     });
     ravens = ravens.filter((raven: Raven) => !raven.markedForDeletion)
-    requestAnimationFrame(animate)
+    if (!gameOver) requestAnimationFrame(animate)
+    if (gameOver) GAME_OVER()
 }
-
 
 animate(0)
