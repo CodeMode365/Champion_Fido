afeb025a2dce61531e97934e25a518c61f3f5440
CodeMode365 <codemode365@gmail.com>
2022-11-20 06:03:07 +0000

    scene complete

--- dist/scene.js
+++ dist/scene.js
@@ -3,7 +3,12 @@ const canvas1 = document.getElementById("sceneCanvas");
 const CTX = canvas1 === null || canvas1 === void 0 ? void 0 : canvas1.getContext("2d");
 const canvasHeight = canvas1 === null || canvas1 === void 0 ? void 0 : canvas1.height = 700;
 const canvasWidth = canvas1 === null || canvas1 === void 0 ? void 0 : canvas1.width = 800;
-let gameSpeed = 5;
+let gameSpeed = 15;
+const slider = document.getElementById('slider');
+const speedOutput = document.getElementById("showGameSpeed");
+slider.addEventListener('change', (e) => {
+    gameSpeed = e.target.value;
+});
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = "../assets/layer-1.png";
 const backgroundLayer2 = new Image();
@@ -14,40 +19,44 @@ const backgroundLayer4 = new Image();
 backgroundLayer4.src = "../assets/layer-4.png";
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = "../assets/layer-5.png";
-let x = 0;
-let x2 = 2400;
-class Layer {
-    constructor(image, speedModifier) {
-        this.x = 0;
-        this.y = 0;
-        this.width = 2400;
-        this.height = 2400;
-        this.x2 = this.width;
-        this.image = image;
-        this.speedModifier = speedModifier;
-        this.speed = gameSpeed * this.speedModifier;
-        this.update();
-        console.log(image);
-    }
-    update() {
-        this.speed = gameSpeed * this.speedModifier;
-        if (this.x <= this.width) {
-            this.x = this.width + this.x2 - this.speed;
+window.addEventListener("load", () => {
+    let x = 0;
+    let x2 = 2400;
+    class Layer {
+        constructor(image, speedModifier) {
+            this.x = 0;
+            this.y = 0;
+            this.width = 2400;
+            this.height = 700;
+            this.image = image;
+            this.speedModifier = speedModifier;
+            this.speed = gameSpeed * this.speedModifier;
         }
-        if (this.x2 <= this.width) {
-            this.x2 = this.width + this.x - this.speed;
+        update() {
+            this.speed = gameSpeed * this.speedModifier;
+            if (this.x <= -this.width) {
+                this.x = 0;
+            }
+            this.x = this.x - this.speed;
         }
-        this.x = Math.floor(this.x - this.speed);
-        this.x2 = Math.floor(this.x2 - this.speed);
+        draw() {
+            CTX.drawImage(this.image, this.x, this.y, this.width, this.height);
+            CTX.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+        }
     }
-    draw() {
-        CTX.drawImage(this.image, this.x, this.y, this.width, this.width);
+    const layer1 = new Layer(backgroundLayer1, .5);
+    const layer2 = new Layer(backgroundLayer2, .4);
+    const layer3 = new Layer(backgroundLayer3, .4);
+    const layer4 = new Layer(backgroundLayer4, .2);
+    const layer5 = new Layer(backgroundLayer5, 1);
+    const gameObject = [layer1, layer2, layer3, layer4, layer5];
+    function animateScene() {
+        CTX.clearRect(0, 0, canvasWidth, canvasHeight);
+        gameObject.forEach(object => {
+            object.update();
+            object.draw();
+        });
+        requestAnimationFrame(animateScene);
     }
-}
-const layer4 = new Layer(backgroundLayer4, 0.5);
-function animateScene() {
-    CTX.clearRect(0, 0, canvasWidth, canvasHeight);
-    layer4.draw();
-    requestAnimationFrame(animateScene);
-}
-animateScene();
+    animateScene();
+});
--- src/scene.ts
+++ src/scene.ts
@@ -3,7 +3,14 @@ const CTX = canvas1?.getContext("2d")
 
 const canvasHeight: number = canvas1?.height = 700
 const canvasWidth: number = canvas1?.width = 800
-let gameSpeed: number = 5;
+let gameSpeed: number = 15;
+// let gameFrame: number = 0
+const slider: (HTMLInputElement | HTMLElement) = document.getElementById('slider')
+const speedOutput = document.getElementById("showGameSpeed")
+slider.addEventListener('change', (e): void => {
+    gameSpeed = e.target.value
+    // speedOutput?.innerText = gameSpeed
+})
 
 //backgrounds
 const backgroundLayer1: HTMLImageElement = new Image()
@@ -17,53 +24,64 @@ backgroundLayer4.src = "../assets/layer-4.png"
 const backgroundLayer5: HTMLImageElement = new Image()
 backgroundLayer5.src = "../assets/layer-5.png"
 
-/* looping the scene */
-let x: number = 0
-let x2: number = 2400
-//Class for each scene 
-class Layer {
-    private x: number
-    private x2: number
-    private y: number
-    private width: number
-    private height: number
-    private image: HTMLImageElement
-    private speedModifier: number
-    private speed: number
-    constructor(image: HTMLImageElement, speedModifier: number) {
-        this.x = 0
-        this.y = 0
-        this.width = 2400
-        this.height = 2400
-        this.x2 = this.width
-        this.image = image
-        this.speedModifier = speedModifier
-        this.speed = gameSpeed * this.speedModifier
-        this.update()
-        console.log(image)
-    }
-    private update(): void {
-        this.speed = gameSpeed * this.speedModifier
-        if (this.x <= this.width) {
-            this.x = this.width + this.x2 - this.speed
-        }
-        if (this.x2 <= this.width) {
-            this.x2 = this.width + this.x - this.speed
-        }
-        this.x = Math.floor(this.x - this.speed)
-        this.x2 = Math.floor(this.x2 - this.speed)
-    }
-    public draw() {
-        CTX.drawImage(this.image, this.x, this.y, this.width, this.width)
-    }
+window.addEventListener("load", () => {
 
-}
-const layer4: Layer = new Layer(backgroundLayer4, 0.5)
-// console.log(layer4.))
+    /* looping the scene */
+    let x: number = 0
+    let x2: number = 2400
+    //Class for each scene 
 
-function animateScene(): void {
-    CTX.clearRect(0, 0, canvasWidth, canvasHeight)
-    layer4.draw()
-    requestAnimationFrame(animateScene)
-}
-animateScene()
+    class Layer {
+        private x: number
+        // private x2: number
+        private y: number
+        private width: number
+        private height: number
+        private image: HTMLImageElement
+        private speedModifier: number
+        private speed: number
+        constructor(image: HTMLImageElement, speedModifier: number) {
+            this.x = 0
+            this.y = 0
+            this.width = 2400
+            this.height = 700
+            // this.x2 = this.width
+            this.image = image
+            this.speedModifier = speedModifier
+            this.speed = gameSpeed * this.speedModifier
+        }
+        public update(): void {
+            this.speed = gameSpeed * this.speedModifier
+
+
+            if (this.x <= -this.width) {
+                this.x = 0
+            }
+            this.x = this.x - this.speed
+            // this.x = gameFrame * this.speed % this.width
+
+        }
+        public draw() {
+            CTX.drawImage(this.image, this.x, this.y, this.width, this.height)
+            CTX.drawImage(this.image, this.x + this.width, this.y, this.width, this.height)
+        }
+
+    }
+    const layer1: Layer = new Layer(backgroundLayer1, .5)
+    const layer2: Layer = new Layer(backgroundLayer2, .4)
+    const layer3: Layer = new Layer(backgroundLayer3, .4)
+    const layer4: Layer = new Layer(backgroundLayer4, .2)
+    const layer5: Layer = new Layer(backgroundLayer5, 1)
+    const gameObject: Layer[] = [layer1, layer2, layer3, layer4, layer5]
+
+    function animateScene(): void {
+        CTX.clearRect(0, 0, canvasWidth, canvasHeight)
+        gameObject.forEach(object => {
+            object.update()
+            object.draw()
+        })
+        // gameFrame--
+        requestAnimationFrame(animateScene)
+    }
+    animateScene()
+})
