1e4a0868731d536b4463dfc06bd559572f707974
CodeMode365 <codemode365@gmail.com>
2022-12-07 14:48:59 +0000

    distance and high score added

--- src/UI.ts
+++ src/UI.ts
@@ -26,9 +26,17 @@ export class UI {
         //score
 
         //timer 
+        ctx.save()
         ctx.font = this.fontSize * 0.8 + 'px ' + this.fontFamily1
         ctx.fillText("Time: " + ((this.game.maxTime - this.game.time) * 0.001).toFixed(1) + "s left", 20, 80)
-        ctx.fillText("Score: " + this.game.score, 20, 50)
+        ctx.fillStyle = "rgb(180,180,50)"
+        ctx.fillText("Coins: " + this.game.coins, 20, 50)
+        ctx.fillStyle = "rgb(170,100,140)"
+        ctx.fillText("Distance Travelled: " + parseInt(this.game.distanceTraveled.toString()) + " m", this.game.width / 2 - 130, 50)
+        ctx.fillStyle = "rgb(140,160,40)"
+        ctx.font = this.fontSize * 0.6 + 'px ' + this.fontFamily2
+        ctx.fillText("High Score: " + this.game.highScore + " m", 20, 20)
+        ctx.restore()
 
         //lives
         for (let i = 0; i < this.game.lives; i++) {
@@ -39,7 +47,10 @@ export class UI {
         if (this.game.gameOver) {
             ctx.save()
             ctx.textAlign = "center"
-            if (this.game.score > this.game.targetScore) {
+            if (this.game.distanceTraveled > this.game.highScore) {
+                localStorage.setItem("highScore", JSON.stringify(parseInt(this.game.distanceTraveled.toString())))
+            }
+            if (this.game.distanceTraveled > this.game.distanceTraveled) {
                 ctx.fillStyle = "Black"
 
                 ctx.font = this.fontSize * 2 + "px " + this.fontFamily1
